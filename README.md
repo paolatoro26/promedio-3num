00dcb81dd2584e7e274590c7da287b4276d3fdce
paola <paolatorobejar@gmail.com>
2016-12-19 17:33:55 -0500

    fin 1.2

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # promedio-3num
+
+Inicio: Promedio de tres números
+## Ingreso de datos
+- numero1
+- numero2
+- numero3
+- suma 
+- promedio
+
+## Procesos
+- pedir numero1
+- pedir numero2
+- pedir numero3
+- sumar numero1 con numero2 y con numero3 
+- almacenar la suma
+- dividir la suma con la cantidad de numeros
+- almacenar el promedio
+
---
+++ main.js
@@ -0,0 +1,13 @@
+	
+	
+	var numero1, numero2, numero3, suma, promedio;
+	numero1 = prompt("por favor , ingresa un numero")
+
+	numero2 = prompt("por favor, ingresa otro numero")
+
+	numero3 = prompt("por favor, ingresa otro numero")
+
+	suma= parseInt(numero1)+parseInt(numero2)+parseInt(numero3);
+	
+	promedio = parseInt(suma/3);
+	alert(promedio);
